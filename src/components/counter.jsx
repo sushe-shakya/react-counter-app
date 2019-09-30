e2f5aaabcc6b5818dfef360734a48f9c252450ac
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-09-30 12:27:17 +0545

    Completed MOSH react counter tutorial
    - Compose components
    - Add nav bars
    - Lift the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,71 +2,43 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
   styles = {
     fontSize: 20,
     fontWeight: "bold"
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p> There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleIncrement(arg) {
-    this.setState({ count: this.state.count + 1 });
-    console.log(arg);
-  }
-
   render() {
     return (
-      //   <div>
-      //     {this.state.tags.length === 0 && "Please create a new tag!"}
-      //     {this.renderTags()}
-      //   </div>
-      <React.Fragment>
+      <div>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
+
         <button
-          onClick={() => this.handleIncrement("clicked")}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
-  state = {
-    counters: [{ id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
-  };
   render() {
     return (
       <div>
-        {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+        <button
+          className="btn btn-primary btn-small-m2"
+          onClick={this.props.onReset}
+        >
+          Reset
+        </button>
+        {this.props.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.props.onDelete}
+            counter={counter}
+            onIncrement={this.props.onIncrement}
+          ></Counter>
         ))}
       </div>
     );
